54b2650c77191b4981a78e0ee190cfc1b5048106
Ciaran Slow <78822538+Nevohteeb@users.noreply.github.com>
2022-10-04 12:03:14 +1300

    Add files via upload

---
+++ JS Basics/script.js
@@ -0,0 +1,28 @@
+
+// Each line is a statement which ends with ;
+// Normally only one statement can run at a time
+// In this case Good Morning, then Good Afternoon, and finally Good Evening
+
+alert("Good Morning!");
+alert("Good Afternoon!");
+alert("Good Evening!");
+
+
+
+// one line comment can be used here
+alert(10); // or you can use here
+
+/* longer comment can be written
+that span over mulitple lines
+by using block comment */
+
+/** 
+ * This style of comment is called a DocBloack, and is often found
+ * in larger projects to serve as a documnetation for the thing that follows it.
+ * Javascript just see a fancy block comment, but other tools will see the
+ * asterisks and read the text and metadata in it and generate helpful books or
+ * pops for you
+ */
+
+// An alert using escape character in the string
+ alert("\"Your attitude determines your direction\"\n - Ralph Waldo Emerson");
