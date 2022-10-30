8ccce1ce41e7c76ad2fb555e3b74e03ae184127e
Ciaran Slow <78822538+Nevohteeb@users.noreply.github.com>
2022-10-31 11:20:37 +1300

    Add files via upload

---
+++ switch-statements/script.js
@@ -0,0 +1,29 @@
+
+// var username = prompt("What's your name?");
+// console.log(username);
+// console.log(username);
+// console.log(username);
+// console.log(username);
+// console.log(username);
+
+var username = prompt("What's your name?");
+var outputCount = 5;
+while (outputCount > 0) {
+    console.log(username);
+    outputCount -= 1; //subtract 1 from outputCOunt and keep new value
+}
+
+// Infinite Loop
+
+// while (true) {
+//     console.log("Yo Toto, I’m really happy for you and Imma let you finish, but Red Bull is the best F1 team of all time. OF ALL TIME… — Kanye West");
+// } 
+
+// For loop
+
+for (var i = 0; i < 10; i += 1) {
+    console.log("I have run " + (i + 1) + " times");
+  }
+
+
+
